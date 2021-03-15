7a6a52ae04b6c724d59a0e48ad5cf55017277a34
Oleg Vaskevich <oleg_vas08@mail.ru>
2021-03-16 02:35:17 +0300

    fix: set file size limit to 50mb

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { StatusCodes, ReasonPhrases } = require('http-status-codes');
 
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use('/favicon.ico', (req, res) => res.sendStatus(StatusCodes.NO_CONTENT));
 
